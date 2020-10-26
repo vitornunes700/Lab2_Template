f5aa06ca9c7ec357b2562111ca9752482344c9f4
patricia.macedo <patricia.macedo@estsetubal.ips.pt>
2020-10-26 16:15:26 +0000

    Initial commit

---
+++ src/pt/pa/Main.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.pa;
+
+import pt.pa.model.BookmarkInvalidOperation;
+
+/**
+ *
+ * @author brunomnsilva
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*     take comments when BookmarkManager is implemented
+        try {
+            BookmarkManager manager = new BookmarkManager();
+            
+            manager.addBookmarkFolder("bookmarks", "Jornais");
+            manager.addBookmarkFolder("jornais", "Finanças");
+            manager.addBookmarkFolder("bookmarks", "Redes Sociais");
+            manager.addBookmarkFolder("bookmarks", "Diversos");
+            
+            manager.addBookmarkEntry("jornais", "Publico", "http://www.publico.pt");
+            manager.addBookmarkEntry("jornais", "Expresso", "http://www.expresso.pt");
+            manager.addBookmarkEntry("finanças", "Diário Económico", "http://economico.sapo.pt/");
+            
+            manager.addBookmarkEntry("redes sociais", "Facebook", "http://www.facebook.com");
+            manager.addBookmarkEntry("redes sociais", "Instagram", "http://www.instagram.com");
+            
+            manager.addBookmarkEntry("diversos", "Gmail", "http://www.gmail.com");
+            manager.addBookmarkEntry("diversos", "StackOverflow", "http://www.stackoverflow.com");
+            
+            manager.addBookmarkEntry("bookmarks", "IPS", "http://www.ips.pt");
+
+            System.out.println(manager);
+
+            System.exit(0);
+
+        } catch (BookmarkInvalidOperation exception) {
+            System.err.println(exception.getMessage());
+        }
+
+         */
+    }
+    
+}
---
+++ test/pt/pa/model/BookmarkManagerTest.java
@@ -0,0 +1,32 @@
+package pt.pa.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookmarkManagerTest {
+    /* take comments when BookmarkManager is implemented
+    private BookmarkManager manager;
+    @BeforeEach
+    void setUp() {
+       manager= new BookmarkManager();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTotalEntries() {
+        assertEquals(0,manager.getTotalEntries());
+        manager.addBookmarkFolder("bookmarks", "Redes Sociais");
+        manager.addBookmarkFolder("bookmarks", "Diversos");
+        assertEquals(2,manager.getTotalEntries());
+        manager.addBookmarkEntry("diversos", "Gmail", "http://www.gmail.com");
+        manager.addBookmarkEntry("diversos", "StackOverflow", "http://www.stackoverflow.com");
+        assertEquals(4,manager.getTotalEntries());
+    }
+
+    */
+
+
+
+
+}
